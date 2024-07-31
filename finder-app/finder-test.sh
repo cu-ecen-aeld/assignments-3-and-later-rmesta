42b335ea046142fe8443ffbcbc53346b411064f7
Rick Mesta <rick.mesta@1791.com>
2024-07-31 17:10:26 -0500

    AELD: Support for Solution to Assignment 4 (Part 2)
    Signed-off-by: Rick Mesta <rick.mesta@1791.com>

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -8,14 +8,29 @@ set -u
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
+
+#
+# Rick Mesta
+# 07/31/2024
+#
+# University of Colorado at Boulder
+# ECEN 5713: Advanced Embedded Linux Development
+# Assignment 4 (Part 2)
+#
+WRITER=/usr/bin/writer
+FINDER=/usr/bin/finder.sh
+CFGDIR=/etc/finder-app/conf
+SAVEFILE="/tmp/assignment4-result.txt"
+SAVECMD="tee -a ${SAVEFILE} 2>&1"
+
+username=$(cat ${CFGDIR}/username.txt)
 
 if [ $# -lt 3 ]
 then
-	echo "Using default value ${WRITESTR} for string to write"
+	echo "Using default value ${WRITESTR} for string to write" | ${SAVECMD}
 	if [ $# -lt 1 ]
 	then
-		echo "Using default value ${NUMFILES} for number of files to write"
+		echo "Using default value ${NUMFILES} for number of files to write" | ${SAVECMD}
 	else
 		NUMFILES=$1
 	fi	
@@ -27,12 +42,12 @@ fi
 
 MATCHSTR="The number of files are ${NUMFILES} and the number of matching lines are ${NUMFILES}"
 
-echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
+echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}" | ${SAVECMD}
 
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=`cat ../conf/assignment.txt`
+assignment=`cat ${CFGDIR}/assignment.txt`
 
 if [ $assignment != 'assignment1' ]
 then
@@ -43,7 +58,7 @@ then
 	#This issue can also be resolved by using double square brackets i.e [[ ]] instead of using quotes.
 	if [ -d "$WRITEDIR" ]
 	then
-		echo "$WRITEDIR created"
+		echo "$WRITEDIR created" | ${SAVECMD}
 	else
 		exit 1
 	fi
@@ -51,10 +66,10 @@ fi
 
 for i in $( seq 1 $NUMFILES)
 do
-	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	${WRITER} "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(${FINDER} "$WRITEDIR" "$WRITESTR" | ${SAVECMD})
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
@@ -62,9 +77,9 @@ rm -rf /tmp/aeld-data
 set +e
 echo ${OUTPUTSTRING} | grep "${MATCHSTR}"
 if [ $? -eq 0 ]; then
-	echo "success"
+	echo "success" | ${SAVECMD}
 	exit 0
 else
-	echo "failed: expected  ${MATCHSTR} in ${OUTPUTSTRING} but instead found"
+	echo "failed: expected  ${MATCHSTR} in ${OUTPUTSTRING} but instead found" | ${SAVECMD}
 	exit 1
 fi
